feb15a51584b6bb77db1f6dff9a1bf21c37695b6
laumi <laumi@michaels-air.fios-router.home>
2019-11-03 21:22:53 -0500

    test

---
+++ src/main/java/com/lau/test1/java_project1/App.java
@@ -0,0 +1,13 @@
+package com.lau.test1.java_project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
